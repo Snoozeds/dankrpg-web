a0fa8f6f117d314aef48e1944fc8f476bf90d68e
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-08-24 21:09:43 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={`DankRPG Docs`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
