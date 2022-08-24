868a1b95e81d5fff9abf1f1fef0426cae6b5ca55
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-08-24 23:36:36 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={`Home`}
       description="DankRPG is an economy bot that prioritises privacy and user-experience.">
       <HomepageHeader />
       <main>
