d38fc3d9d4b83c270f9d7e74a439e87961a0066f
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-08-26 23:46:13 +0100

    Try adding Algolia.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,4 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
@@ -14,14 +13,9 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Snoozeds', // Usually your GitHub org/user name.
-  projectName: 'dankrpg-docs', // Usually your repo name.
+  organizationName: 'Snoozeds',
+  projectName: 'dankrpg-docs',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -34,8 +28,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -121,12 +113,22 @@ const config = {
               }
             ],
           },
+
         ],
         copyright: `Copyright © ${new Date().getFullYear()} DankRPG, built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        appId: 'WY8FLWV5YJ',
+        apiKey: 'e7a7b9b2670afb46e220ca5b016a40f1', // public api key.
+        indexName: 'dankrpg',
+        contextualSearch: true,
+        externalUrlRegex: 'external\\.com|domain\\.com',
+        searchParameters: {},
+        searchPagePath: 'search',
       },
     }),
 };
